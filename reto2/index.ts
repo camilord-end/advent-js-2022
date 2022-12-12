92e191dc36eb39b1317a103c7e54351933ab8fe1
Julian Orozco <juliancaor10@gmail.com>
2022-12-12 14:14:58 -0500

    update challenge 2

--- reto2/index.ts
+++ reto2/index.ts
@@ -2,7 +2,7 @@ export function countHours(year: number, holidays: string[]): number {
   let hours: number = 0
   holidays.forEach((holiday) => {
     let holidayDate = new Date(`${year}/${holiday}`)
-    if (!(holidayDate.getDay() === 0) && !(holidayDate.getDay() === 6)) {
+    if (holidayDate.getDay() % 6) {
       hours = hours + 2
     }
   })
