d1b1f62a3ac4ee27a93a73cdb39eee44f681fd7b
Julian Orozco <juliancaor10@gmail.com>
2022-12-15 13:54:04 -0500

    Add challenge 15 solution

---
+++ reto15/index.ts
@@ -0,0 +1,24 @@
+export function decorateTree(base: string) {
+  const t: any = {
+    BB: 'B',
+    BP: 'R',
+    BR: 'P',
+    PP: 'P',
+    PR: 'B',
+    PB: 'R',
+    RR: 'R',
+    RB: 'P',
+    RP: 'B'
+  }
+  let sol: string[] = []
+  sol[0] = base
+  while (sol[0].length > 1) {
+    let itemArr = sol[0].split(' ')
+    let eq = itemArr.map((element, index, a) => {
+      return t[element + a[index + 1]]
+    })
+    sol.unshift(eq.filter((e) => e).join(' '))
+  }
+  return sol
+}
+console.log(decorateTree('B P R P'))
